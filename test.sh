80b108fc69c78d7f4d5c8ae398f9b8887fb50af6
Emilio Heinzmann <emiliosheinz@gmail.com>
2025-07-13 09:19:39 -0300

    chore: add test files and script

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker compose down --volumes --remove-orphans
+docker compose up -d --build --force-recreate
+
+K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_OPEN=true k6 run ./rinha-test/rinha.js
