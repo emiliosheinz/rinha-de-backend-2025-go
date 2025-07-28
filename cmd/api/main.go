85791a02e84b6c09a30a05e4704e374bba0826df
Emilio Heinzmann <emiliosheinz@gmail.com>
2025-07-28 08:47:12 -0300

    feat: increase concurrency back to normal

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -49,7 +49,7 @@ func startQueueWorkersServer(paymentsService *payments.PaymentsService) {
 	srv := asynq.NewServerFromRedisClient(
 		database.RedisClient,
 		asynq.Config{
-			Concurrency:              16,
+			Concurrency:              32,
 			DelayedTaskCheckInterval: time.Duration(500) * time.Millisecond,
 			RetryDelayFunc: func(n int, e error, t *asynq.Task) time.Duration {
 				return time.Duration(3) * time.Second
